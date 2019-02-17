f2771f815c8fa76f9cedbdaa2b0000795c8545aa
Surbma <peter@surbma.hu>
2019-02-17 22:15:34 +0100

    11.0
    - Elmaradt verziószám frissítés.

--- surbma-magyar-woocommerce.php
+++ surbma-magyar-woocommerce.php
@@ -5,7 +5,7 @@ Plugin Name: HuCommerce - Magyar WooCommerce kiegészítések
 Plugin URI: https://www.hucommerce.hu/
 Description: Hasznos javítások a magyar nyelvű WooCommerce webáruházakhoz.
 
-Version: 10.1
+Version: 11.0
 
 Author: HuCommerce
 Author URI: https://www.hucommerce.hu/
