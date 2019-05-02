3bc2ec80e231e06c4388e8ddf03ec58db97aef35
Viktor Szépe <viktor@szepe.net>
2019-05-02 09:46:46 +0200

    Declare min_amounts + early bail out
    `Undefined variable: min_amounts`

--- modules/free-shipping-notice.php
+++ modules/free-shipping-notice.php
@@ -9,6 +9,7 @@ function surbma_hc_free_shipping_cart_notice() {
 	// Get Free Shipping Methods for Rest of the World Zone & populate array $min_amounts
 	$default_zone = new WC_Shipping_Zone(0);
 	$default_methods = $default_zone->get_shipping_methods();
+	$min_amounts = array();
 	foreach( $default_methods as $key => $value ) {
 		if ( $value->id === 'free_shipping' ) {
 			if ( $value->min_amount > 0 ) $min_amounts[] = $value->min_amount;
@@ -25,20 +26,22 @@ function surbma_hc_free_shipping_cart_notice() {
 		}
 	}
 
+	if ( empty( $min_amounts ) ) {
+		return;
+	}
+
 	// Find lowest min_amount
-	if ( is_array( $min_amounts ) ) {
-		$min_amount = min( $min_amounts );
+	$min_amount = min( $min_amounts );
 
-		// Get Cart Subtotal inc. Tax excl. Shipping
-		$current = WC()->cart->subtotal;
+	// Get Cart Subtotal inc. Tax excl. Shipping
+	$current = WC()->cart->subtotal;
 
-		// If Subtotal < Min Amount Echo Notice and add "Continue Shopping" button
-		if ( $current < $min_amount ) {
-			$message = $freeshippingnoticemessageValue . ': ' . wc_price( $min_amount - $current );
-			$returnurl = esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) );
-			$notice = sprintf( '%s <a href="%s" class="button wc-forward">%s</a>', $message, $returnurl, esc_html__( 'Return to shop', 'woocommerce' ) );
-			wc_print_notice( $notice, 'notice' );
-		}
+	// If Subtotal < Min Amount Echo Notice and add "Continue Shopping" button
+	if ( $current < $min_amount ) {
+		$message = $freeshippingnoticemessageValue . ': ' . wc_price( $min_amount - $current );
+		$returnurl = esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) );
+		$notice = sprintf( '%s <a href="%s" class="button wc-forward">%s</a>', $message, $returnurl, esc_html__( 'Return to shop', 'woocommerce' ) );
+		wc_print_notice( $notice, 'notice' );
 	}
 }
 add_action( 'woocommerce_before_cart', 'surbma_hc_free_shipping_cart_notice' );
